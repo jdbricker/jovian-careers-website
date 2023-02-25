5895d80b91aebe109b7e696832006c89f506847a
Jeff Bricker <jdbricker@gmail.com>
2023-02-25 10:08:41 -0500

    changes 2 a couple of filez

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+#at commmand line run with 'python app.py' otherwise will run as module and name != main
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['DEBUG']=True
 
 @app.route('/')
 def hello_world():
-    #
+    #return "return value"
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
